49c78f5f96446270827fbd9f9db135af87522033
Ana <anarita.dauane@gmail.com>
2020-05-14 10:39:13 +0200

    oware commit 1

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -4,17 +4,28 @@ type StartingPosition =
     | South
     | North
 
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n board = List.item(n-1) board
 
-let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+let useHouse n board =  
+  let _ = getSeeds n board
+                      //let num = getSeeds n board 
+                      
+                      
+                        
+
+
+
+
+let start position = 
+                    match position with 
+                    | South -> [4;4;4;4;4;4;4;4;4;4;4;4]
+                    | _ -> [4;4;4;4;4;4;4;4;4;4;4;4] 
 
 let score board = failwith "Not implemented"
 
 let gameState board = failwith "Not implemented"
 
 [<EntryPoint>]
-let main _ =
-    printfn "Hello from F#!"
-    0 // return an integer exit code
+let main _ = 0 // return an integer exit code
+    
